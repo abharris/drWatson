dccfcc03c847ac21d9b5e7c655da8ed987879a14
alexisbh <alexisbharris@gmail.com>
2019-04-10 22:28:06 -0400

    finished v1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
 console.log('starting up!');
+
+let seconds = 0;
+
+setInterval(() => {
+  $('#main').html(`You've been on this page for ${seconds} seconds`);
+  seconds += 1;
+}, 1000);
