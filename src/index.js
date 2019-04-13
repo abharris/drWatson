a11e9317529b8a4a0231c18dd36a86141e475d99
alexisbh <alexisbharris@gmail.com>
2019-04-13 14:34:44 -0400

    started react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-// change require to es6 import style
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-console.log('starting up!');
+const App = () => <div className="test">All the REACT are belong to us!</div>;
 
-let seconds = 0;
-
-setInterval(() => {
-  $('#main').html(`You've been on this page for ${seconds} seconds`);
-  seconds += 1;
-}, 1000);
+ReactDOM.render(<App />, document.getElementById('main'));
